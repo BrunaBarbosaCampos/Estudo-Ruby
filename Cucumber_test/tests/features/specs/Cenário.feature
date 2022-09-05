15f1736d7c2a40e3d52d89fae48d10eb307dbe20
Bruna Barbosa Campos <brunanataliacampos12602@gmail.com>
2022-09-05 11:05:23 -0300

    Soma e subtração

---
+++ Cucumber_test/tests/features/specs/Cenário.feature
@@ -0,0 +1,18 @@
+#language: pt
+
+
+Funcionalidade: Calculo e subtração e soma. 
+
+Cenario: Fazer subtração
+Dado que o usuário tenha 200 reais. 
+Quando o usuário gasta 100 reais.
+Então o usuário verifica quanto dinheriro restou.
+
+
+Cenario: Fazer soma 
+Dado que o usuário tenha 5 carros. 
+Quando o usuário compra mais 2 carros.
+Então o usuário conta quantos usuários tem ao todo. 
+
+
+
---
+++ Cucumber_test/tests/features/step_definitions/Cenário.rb
@@ -0,0 +1,45 @@
+
+    
+Dado('que o usuário tenha {int} reais.') do |inteiro1|
+    p inteiro1
+    @Dinheiro1 = inteiro1
+end
+    
+
+Quando('o usuário gasta {int} reais.') do |inteiro2|
+    p inteiro2
+    @Gastar = inteiro2
+    @Resultado = @Dinheiro1 - @Gastar
+end
+   
+ 
+
+Então("o usuário verifica quanto dinheriro restou.") do
+    p @Resultado
+    expect(@Resultado).to eq 100
+end
+
+
+
+
+Dado('que o usuário tenha {int} carros.') do |carro1|
+ p carro1
+ @Carro1 = carro1
+end
+
+
+
+Quando('o usuário compra mais {int} carros.') do |carro2|
+    p carro2
+    @Carro2 = carro2
+    @CarroTotal = @Carro1 + @Carro2
+end
+    
+    
+
+Então('o usuário conta quantos usuários tem ao todo.') do
+    p @CarroTotal
+    expect(@CarroTotal).to eq 7
+
+end
+ 
